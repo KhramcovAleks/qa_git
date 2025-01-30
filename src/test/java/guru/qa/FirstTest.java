c577d973c2d58c20569cd4691ea01f70ca567349
Александр Храмков <aleksandr.khramkov@simbirsoft.com>
2025-01-30 21:45:23 +0400

    Fixes four test - комментарий

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
-
+//добавил комментарий
     @Test
     void firstTest(){
         Assertions.assertTrue(true);
