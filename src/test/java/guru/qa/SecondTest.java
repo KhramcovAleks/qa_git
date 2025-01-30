81005a570c6689ee11c8f4d95a7bc3ffb1b3169e
Александр Храмков <aleksandr.khramkov@simbirsoft.com>
2025-01-30 17:58:06 +0400

    Added second test

---
+++ src/test/java/guru/qa/SecondTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+    @Test
+    void secondTest() {
+
+
+        Assertions.assertTrue(true);
+    }
+}
